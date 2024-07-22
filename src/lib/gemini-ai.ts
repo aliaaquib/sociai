4697fdd8dc50298a12792b917fd76ffa71c8bbd8
aliaaquib <imaaquibali@gmail.com>
2024-07-23 00:48:12 +0600

    gemini api key updated

--- src/lib/gemini-ai.ts
+++ src/lib/gemini-ai.ts
@@ -1,12 +1,22 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+// const apiKey = process.env.GEMINI_API_KEY;
+// const genAI = new GoogleGenerativeAI(apiKey);
+
+// const model = genAI.getGenerativeModel({
+//   model: "gemini-1.5-flash",
+// });
+
 const apiKey = process.env.GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error('GEMINI_API_KEY is not defined');
+}
+
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
-
 
 const generationConfig = {
   temperature: 1,
