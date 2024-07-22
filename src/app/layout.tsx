2eb29aea757480e6e635d05ea4d7010a8a84a8a9
aliaaquib <imaaquibali@gmail.com>
2024-07-23 00:43:52 +0600

    soci ai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Writer Ai",
+  title: "Soci Ai",
   description: "Effortlessly create engaging, high-quality social media posts with our AI-driven platform",
 };
 
